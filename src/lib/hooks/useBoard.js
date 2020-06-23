212f0774564bc3a7d4a40d671157f09b77c585ab
Shay Cojocaru <shayc@outlook.com>
2020-06-24 00:45:23 +0300

    .

--- src/lib/hooks/useBoard.js
+++ src/lib/hooks/useBoard.js
@@ -37,7 +37,7 @@ function useBoard(boardSet = initialBoardSet, deps = initialDeps) {
       buttonCount: buttons.length,
     };
   });
-console.log('boardList', boardList)
+
   const rootBoardId = getRootBoard()?.id;
   const nav = useNavigation([rootBoardId], 0);
   const board = nav.currentHistory ? getBoardById(nav.currentHistory) : {};
