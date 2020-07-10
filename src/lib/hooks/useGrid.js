438d0ca95d334c44ba2551d4b1ca8ed282a94d49
Shay Cojocaru <shayc@outlook.com>
2020-07-11 00:01:04 +0300

    .

--- src/lib/hooks/useGrid.js
+++ src/lib/hooks/useGrid.js
@@ -5,7 +5,30 @@ function useGrid(grid) {
   const [rows, setRows] = useState(grid.rows);
   const [order, setOrder] = useState(grid.order);
 
-  return { columns, order, rows, setColumns, setOrder, setRows };
+  function moveItem(item, cell) {
+    const mappedOrder = order.map((row) => {
+      return row.map((cell) => {
+        return cell;
+      });
+    });
+
+    const oldItem = mappedOrder[cell.row][cell.column];
+
+    for (let i = 0; i < mappedOrder.length; i++) {
+      const itemIndex = mappedOrder[i].indexOf(item.id);
+
+      if (itemIndex !== -1) {
+        mappedOrder[i][itemIndex] = oldItem;
+        break;
+      }
+    }
+
+    mappedOrder[cell.row][cell.column] = item.id;
+
+    setOrder(mappedOrder);
+  }
+
+  return { columns, moveItem, order, rows, setColumns, setOrder, setRows };
 }
 
 export default useGrid;
